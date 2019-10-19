843f94d1093acec185cb107bc31e76b5be62b738
Olayinka Ola <mimoyinka@gmail.com>
2019-10-19 11:24:44 +0100

    Automation and Transpiling

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+let port = 4000;
+let app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err) => {
+  if(err){
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+})
